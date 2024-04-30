6c13c9ee872eb1d1b323e33f8a352b49b99487de
Romano Ibarra <romanoibarra12@gmail.com>
2024-04-29 21:27:58 -0300

    Diseño datalist with search component

--- create-segmentation.js
+++ create-segmentation.js
@@ -68,3 +68,123 @@ showEntries.addEventListener('click', (e) => {
 
     entriesList.classList.remove('hidden');
 })
+
+class Selector {
+    constructor (containerEl = {}, options = [{name: '', value: '', selected: false, optionEl: {}}]) {
+        this.containerEl =  containerEl;
+        this.options = options;
+        this.optionsEl = {};
+        this.inputEl = this.containerEl.querySelector('.component-input');
+    }
+
+    renderSelector() {
+        const optionsEl = document.createElement('div');
+        this.optionsEl = optionsEl;
+
+        optionsEl.classList.add('options');
+
+        this.containerEl.append(optionsEl);
+
+        this.renderOptions();
+    }
+
+    renderOptions() {
+        this.optionsEl.innerHTML = "";
+
+        this.options.forEach(option => {
+            const optionEl = document.createElement('div');
+
+            optionEl.classList.add('option');
+
+            if(option.selected) optionEl.classList.add('active');
+            optionEl.innerHTML = `
+                <div class="device-check">
+                    <span class="check-icon"></span>
+                </div>
+                ${option.name}
+            `;
+            optionEl.id = option.value;
+
+            this.optionsEl.append(optionEl);
+            option.optionEl = optionEl;
+        })
+
+        this.addEvents();
+    }
+
+    addEvents() {
+        this.optionsEl.addEventListener('click', event => {
+            this.optionsEl.classList.add('open');
+        })
+
+        this.options.forEach(option => {
+            option.optionEl.addEventListener('click', (event) => {
+                event.preventDefault();
+
+                option.selected = !option.selected;
+
+                option.optionEl.classList.toggle('active');
+            })
+        })
+
+        document.addEventListener('click', event => {
+            let children = [
+                ...this.optionsEl.childNodes
+            ];
+
+            if(children.some(child => event.target.id === child.id)) return this.optionsEl.classList.add('open');
+            
+            if(event.target !== this.optionsEl 
+            && event.target !== this.containerEl.querySelector('.input-container')
+            && event.target !== this.inputEl) this.optionsEl.classList.remove('open')
+
+        })
+
+        this.containerEl.querySelector('.input-container').addEventListener('click', event => {
+            this.optionsEl.classList.add('open');
+        })
+
+        this.inputEl.addEventListener('input', event => {
+            this.renderSearchOptions(event.target.value.toLowerCase());
+        })
+    }
+
+    renderSearchOptions(search) {
+        this.options.forEach(option => {
+            option.optionEl.classList.remove('hidden');
+
+            if(!option.name.trim().toLocaleLowerCase().includes(search)) return option.optionEl.classList.add('hidden');
+        })
+    }
+
+    getSelectedOptions() {
+        return this.options.filter(option => option.selected === true);
+    }
+}
+
+const selectorCity = new Selector(document.getElementById('selector-city'), [
+    {name: 'Hagfors', value: 'hagfors', selected: false, optionEl: {}},
+    {name: 'Halmstad', value: 'halmstad', selected: false, optionEl: {}},
+    {name: 'Haparanda', value: 'haparanda', selected: true, optionEl: {}}
+]);
+
+selectorCity.renderSelector();
+
+const selectorMunicipality = new Selector(document.getElementById('selector-municipality'), [
+    {name: 'Municipality 1', value: 'municipality1', selected: false, optionEl: {}},
+    {name: 'Municipality 2', value: 'municipality2', selected: false, optionEl: {}},
+    {name: 'Municipality 3', value: 'municipality3', selected: true, optionEl: {}},
+    {name: 'Municipality 4', value: 'municipality4', selected: false, optionEl: {}},
+    {name: 'Municipality 5', value: 'municipality5', selected: false, optionEl: {}},
+    {name: 'Municipality 6', value: 'municipality6', selected: true, optionEl: {}}
+]);
+
+selectorMunicipality.renderSelector();
+
+const selectorProduct = new Selector(document.getElementById('selector-product'), [
+    {name: 'Product 1', value: 'product1', selected: false, optionEl: {}},
+    {name: 'Product 2', value: 'product2', selected: false, optionEl: {}},
+    {name: 'Product 3', value: 'product3', selected: true, optionEl: {}}
+]);
+
+selectorProduct.renderSelector();
